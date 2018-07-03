03cd11fd5c193171d25860c2489c706c05455941
Kamyar Nemati <kamyarnemati@gmail.com>
2018-07-03 11:48:00 +0800

    Home page lists registered users as contacts.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Contacts</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +14,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @foreach ($contacts as $contact)
+                        <p><a href="{{ $contact['uuid'] }}"><b>{{ $contact['name'] }}</b>&nbsp;({{ $contact['email'] }})</a></p>
+                    @endforeach
                 </div>
             </div>
         </div>
